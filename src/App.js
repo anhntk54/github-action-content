18e964c1d4d204953ae30df9ab7c12af782a8565
anhnhu <anhnt@subwallet.app>
2023-11-22 17:35:12 +0700

    tesst vesion

--- src/App.js
+++ src/App.js
@@ -5,14 +5,6 @@ function App() {
     <div className="App">
       <header className="App-header">
         Build Number: {pkgJson.buildNumber}
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
