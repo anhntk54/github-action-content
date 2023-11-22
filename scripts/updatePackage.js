a61a050fb8ae7d8db395963e36026941424707d8
anhnhu <anhnt@subwallet.app>
2023-11-22 14:35:13 +0700

    test build number

---
+++ scripts/updatePackage.js
@@ -0,0 +1,13 @@
+const {ApiPromise, WsProvider} = require('@polkadot/api');
+const fs = require('fs/promises');
+const pkgJson = require('../package.json');
+
+async function main() {
+    pkgJson.buildNumber = parseInt(pkgJson.buildNumber) + 1;
+    console.log(pkgJson)
+    await fs.writeFile('package.json', JSON.stringify(pkgJson, null, 2));
+    await fs.writeFile('a.json', JSON.stringify(pkgJson, null, 2));
+    console.log(`You are update build number to ${pkgJson.buildNumber} `)
+}
+
+main().catch(console.error).finally(() => process.exit());
