f7b4b0538b93dbc897b343f94408c725989deb21
anhnhu <anhnt@subwallet.app>
2024-01-13 17:21:51 +0700

    remove content

--- scripts/fetch-data-content.mjs
+++ scripts/fetch-data-content.mjs
@@ -7,68 +7,16 @@ const STRAPI_URL = 'https://37b6-14-181-208-33.ngrok-free.app';
 const RESOURCE_URL = 'https://static-data.subwallet.app';
 const savePath = (folder, fileName) => `data/${folder}/${fileName || 'list.json'}`;
 const savePathFolder = (folder, fileName) => `data/${folder}`;
-const saveImagesPath = (folder) => `data/${folder}/images`;
-const urlImage = (folder, field, name) => `${RESOURCE_URL}/${folder}/images/${field}/${name}`;
-
-export async function downloadFile(url, downloadDir, forceFileName = null) {
-    // Create dir if not exist
-    if (!fs.existsSync(downloadDir)) {
-        fs.mkdirSync(downloadDir, {recursive: true});
-    }
-
-    // Download file with axios
-    let fileName = url.split('/').pop();
-    if (forceFileName) {
-        fileName = forceFileName + '.' + fileName.split('.').pop();
-    }
-    const filePath = path.join(downloadDir, fileName);
-
-    // Download and save file
-    const writer = fs.createWriteStream(filePath);
-
-    const response = await axios({
-        url,
-        method: 'GET',
-        responseType: 'stream'
-    });
-
-    response.data.pipe(writer);
-
-    return await new Promise((resolve, reject) => {
-        writer.on('finish', () => {
-            resolve(fileName);
-        });
-        writer.on('error', () => {
-            resolve(url);
-        });
-    });
-}
 
 const fetchAndProcessData = async (url) => {
     const results = await axios.get(url);
-    const data = results.data;
-    return data;
-    // if (!results.data) return;
-    //
-    // return await Promise.all(results.data.map(async item => {
-    //     const dataImages = {};
-    //     for (const field of fieldsImage) {
-    //         const dataField = item[field];
-    //         if (dataField) {
-    //             const folderFieldImage = `${downloadDir}/${field}`;
-    //             const newFileName = await downloadFile(dataField, folderFieldImage);
-    //             dataImages[field] = urlImage(folder, field, newFileName);
-    //         }
-    //     }
-    //     return {...item, ...dataImages};
-    // }));
+    return results.data;
 }
 const createData = async (folderName, fileName, content) => {
         const folderPath = savePathFolder(folderName);
         if (!fs.existsSync(folderPath)) {
             fs.mkdirSync(folderPath, {recursive: true});
         }
-        // const fileName = `${item.slug}.json`;
         const filePath = savePath(folderName, fileName);
         await writeFile(filePath, content);
 }
@@ -84,7 +32,7 @@ const main = async () => {
         if (value.length > 0) {
             value.forEach((item) => {
                 const {folderName, locale, content} = item;
-            const fileName = `${locale}${isProduction ? '' : 'preview'}.md`;
+            const fileName = `${locale}${isProduction ? '' : '-preview'}.md`;
             console.log(folderName, fileName)
             acc.push(createData(folderName, fileName, content));
             })
