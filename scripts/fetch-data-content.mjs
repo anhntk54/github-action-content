7aaba24203169a4878c2cb9da955010f42ec63ec
anhnhu <anhnt@subwallet.app>
2024-01-19 18:03:06 +0700

    new data

--- scripts/fetch-data-content.mjs
+++ scripts/fetch-data-content.mjs
@@ -2,7 +2,7 @@ import * as fs from "fs";
 import axios from "axios";
 import {writeFile} from "./utils.mjs";
 
-const STRAPI_URL = 'https://9ef8-14-181-208-33.ngrok-free.app';
+const STRAPI_URL = 'https://b063-14-181-208-33.ngrok-free.app';
 const savePath = (folder, fileName) => `data/markdown-content/${folder}/${fileName}`;
 const savePathFolder = (folder) => `data/markdown-content/${folder}`;
 
@@ -19,20 +19,35 @@ const createContentToFile = async (folderName, fileName, content) => {
     await writeFile(filePath, content);
 }
 
+const createContentToJSONFile = async (folderName, fileName, content, locale) => {
+
+}
+
 const main = async () => {
     console.log('Start fetching data...');
     const url = `${STRAPI_URL}/api/list/markdown-content`
     const dataContent = await fetchAndProcessData(url);
+    // console.log(dataContent)
     const environment = process.argv[2];
     const isProduction = environment === 'production';
-    const promiseList = Object.entries(dataContent).reduce((acc, [key, value]) => {
-        if (value.length > 0) {
-            value.forEach((item) => {
-                const {folderName, locale, content} = item;
-                const fileName = `${locale}${isProduction ? '' : '-preview'}.md`;
-                acc.push(createContentToFile(folderName, fileName, content));
-            })
+    const promiseList = dataContent.reduce((acc, value) => {
+        const {folder, locale, localizations, content, description} = value;
+        const contentSave = {
+            content, description
         }
+        const prefix = isProduction ? 'list-' : 'preview-';
+        const fileName = `${prefix}${locale}.json`;
+        if (localizations && localizations.length > 0) {
+            for (const localization of localizations) {
+                const {locale, content, description} = localization;
+                const fileName = `${prefix}${locale}.json`;
+                const contentSave = {
+                    content, description
+                }
+                acc.push(createContentToFile(folder, fileName, JSON.stringify(contentSave, null, 2)));
+            }
+        }
+        acc.push(createContentToFile(folder, fileName, JSON.stringify(contentSave, null, 2)));
         return acc;
     }, [])
     await Promise.all(promiseList);
